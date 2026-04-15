2d4cb90da191042b3a10f80970b9374918d0f00c
sisiam08 <mohammadsisiam08@gmail.com>
2026-04-16 00:41:53 +0600

    Feat: add middleware

---
+++ src/middleware/auth.ts
@@ -0,0 +1,50 @@
+import { NextFunction, Request, Response } from "express";
+import { UserRole } from "../../generated/prisma/enums";
+import { auth } from "../lib/auth";
+
+export const auth_middleware = (role: UserRole[]) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const session = await auth.api.getSession({
+        headers: req.headers as any,
+      });
+
+
+      if (!session || !session.user) {
+        res.status(401).json({
+          success: false,
+          message: "Unauthorized",
+        });
+        return;
+      }
+
+      if (!session.user.emailVerified) {
+        res.status(403).json({
+          success: false,
+          message: "You need to verify your email to access this resource",
+        });
+        return;
+      }
+
+      if (role.length && !role.includes(session.user.role as UserRole)) {
+        res.status(403).json({
+          success: false,
+          message: "You don't have permission to access this resource",
+        });
+        return;
+      }
+
+      req.user = {
+        id: session.user.id as string,
+        name: session.user.name as string,
+        email: session.user.email as string,
+        role: session.user.role as UserRole,
+        emailVerified: session.user.emailVerified as boolean,
+      };
+
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
+};
--- src/modules/TutorProfiles/tutorProfile.router.ts
+++ src/modules/TutorProfiles/tutorProfile.router.ts
@@ -1,13 +1,23 @@
 import { Router } from "express";
 import { TutorProfileControllers } from "./tutorProfile.controller";
+import { auth_middleware } from "../../middleware/auth";
+import { UserRole } from "../../../generated/prisma/enums";
 
 const router = Router();
 
-router.post("/", TutorProfileControllers.createProfile);
+router.post(
+  "/",
+  auth_middleware([UserRole.TUTOR]),
+  TutorProfileControllers.createProfile,
+);
 
 router.get("/", TutorProfileControllers.getAllProfiles);
 
-router.get("/profile", TutorProfileControllers.getMyProfile);
+router.get(
+  "/profile",
+  auth_middleware([UserRole.TUTOR]),
+  TutorProfileControllers.getMyProfile,
+);
 
 router.get("/:id", TutorProfileControllers.getProfileById);
 
